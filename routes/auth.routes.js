0c3c80d6ffef2324e06e0d694a603ead66994c40
Ivan <Ivandgea@gmail.com>
2023-03-09 15:33:14 +0100

    ivan back jueves9

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const fileUploader = require('../config/cloudinary.config');
+// const fileUploader = require('../config/cloudinary.config');
 
 // ℹ️ Handles password encryption
 const bcrypt = require("bcrypt");
