4c917b72246500687af2407210665b210624de86
Eduardo Pizarro <eduardo-pizarro@hotmail.es>
2023-03-09 15:32:46 +0100

    con cloudinary full power

--- app.js
+++ app.js
@@ -25,7 +25,13 @@ app.use("/auth", authRoutes);
 
 const shipmentRoutes = require("./routes/shipment.routes");
 app.use("/api/shipment", shipmentRoutes)
+
+const moviesRouter = require('./routes/movies.routes'); // <== has to be added
+app.use('/movies', moviesRouter); // <== has to be added
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
+ 
+
 
 module.exports = app;
