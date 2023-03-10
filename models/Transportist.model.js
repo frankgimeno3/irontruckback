d6e98fb74d11813ed5e910b8909ae7c3a4c70413
Ivan <Ivandgea@gmail.com>
2023-03-10 18:07:48 +0100

    commit viernes tarde

--- models/Transportist.model.js
+++ models/Transportist.model.js
@@ -1,6 +1,9 @@
 const { Schema, model } = require("mongoose");
+
 const transportistSchema = new Schema(
+
     {
+
         email: {
             type: String,
             required: [true, "Email is required."],
@@ -8,47 +11,66 @@ const transportistSchema = new Schema(
             lowercase: true,
             trim: true,
         },
+
         name: {
             type: String,
             required: [true, "Name is required."],
         },
+
         password: {
             type: String,
             required: [true, "Password is required."],
         },
+
         image: {
             type: String,
         },
+
         phoneNumber: String,
+
         licensePlate: {
             type: String,
             required: [true, "License Plate is required"],
         },
-        profesionaltype: {
+
+        professionaltype: {
             type: String,
             enum: ["Company", "Autonomous"],
             required: [true, "Name is required."]
         },
+
         company: {
             type: String
         },
+
+
         nif: {
             type: String,
             required: true,
         },
+
         savedShipments: { type: [Schema.Types.ObjectId], ref: "Shipment" },
+
         currentShipments: { type: [Schema.Types.ObjectId], ref: "Shipment" },
+
         completedShipments: { type: [Schema.Types.ObjectId], ref: "Shipment" },
+
         rejectedShipments: { type: [Schema.Types.ObjectId], ref: "Shipment" },
+
         isAdmin: {
             type: Boolean,
             default: false,
         }
     },
+
+
+
     {
         // this second object adds extra properties: `createdAt` and `updatedAt`
         timestamps: true,
     }
 );
+
 const Transportista = model("Comment", transportistSchema);
+
 module.exports = Transportista;
