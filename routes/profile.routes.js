839c5fb189f17ab4c0727a7827f85d8c9aee2c00
Eduardo Pizarro <eduardo-pizarro@hotmail.es>
2023-03-10 17:45:44 +0100

    con desbloqueo rutas profile

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,25 +1,45 @@
 const express = require("express");
 const router = express.Router();
- 
+import { createContext, useEffect, useState } from "react";
+import { AuthContext } from "../../irontruck/src/context/auth.context";
 const User = require("../models/User.model");
- 
+
 // ********* require fileUploader in order to use it *********
 const fileUploader = require("../config/cloudinary.config");
 const { isAuthenticated } = require("../middleware/jwt.middleware");
 
- 
+
+useEffect(() => {
+  authenticateUser();
+}, []);
+
+const {user} = useContext(AuthContext);
+
 // GET "/:id" => Route to your profile
 router.get("/:id", isAuthenticated, (req, res, next) => {
-  const { idProject } = req.params;
-  User.findById({idProject})
+ const { idProject } = user._id;
+ const { licensePlate } = user.licensePlate;
+
+ if(!licensePlate)
+  Sender.findById({idProject})
   .then(result => {
     res.json(result);
 })
 .catch(err => next(err))
+if(licensePlate)
+
+Transportist.findById({idProject})
+.then(result => {
+  res.json(result);
+})
+.catch(err => next(err))
 });
+
+
  
 // PUT /" => Route that receives the image, sends it to Cloudinary via the fileUploader and returns the image URL
 router.put ("/:id", isAuthenticated, fileUploader.single("imageUrl"), (req, res, next) => {
+  const { idProject } = req.params;
   // console.log("file is: ", req.file)
   const { email, name, _id, phoneNumber, address } = req.body;
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
