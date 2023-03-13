7603b1db50579069a00acefbb9df7c8915ef1f57
Ivan <Ivandgea@gmail.com>
2023-03-13 19:56:02 +0100

    registro de shipments done

--- routes/shipment.routes.js
+++ routes/shipment.routes.js
@@ -84,6 +84,57 @@ router.delete("/delete/:id", (req, res, next) => {
     const { idShipment } = req.params;
     Shipment.findByIdAndDelete(idShipment)
         .then(response => {
+            console.log(response)
+
+        })
+        .catch(err => next(err))
+});
+// router.post("/new", (req, res, next) => {
+//     const { author, creationDate, pickUpDireccion, pickUpProvince, deliveryDireccion, deliveryProvince, pallets } = req.body;
+//     console.log(req.body)
+//     Shipment.create({ author, creationDate, pickUpDireccion, pickUpProvince, deliveryDireccion, deliveryProvince, pallets })
+//         // .then(response => {
+//         //     console.log(response)
+
+//         //     // return Sender.findByIdAndUpdate(author, { $push: { createdShipments: response } }, { new: true })
+//         // })
+//         .then((data) => {
+//              res.json(response) })
+
+//         .catch(err => next(err))
+// });
+
+// router.post("/new", (req, res, next) => {
+//     const { author, creationDate, pickUpDireccion, pickUpProvince, deliveryDireccion, deliveryProvince, pallets } = req.body;
+//     console.log(req.body)
+//     Shipment.create({ author, creationDate, pickUpDireccion, pickUpProvince, deliveryDireccion, deliveryProvince, pallets })
+//         // .then(response => {
+//         //     console.log(response)
+
+//         //     // return Sender.findByIdAndUpdate(author, { $push: { createdShipments: response } }, { new: true })
+//         // })
+//         .then((data) => {
+//              res.json(response) })
+
+//         .catch(err => next(err))
+// });
+
+
+router.put("/edit/:idshipment", (req, res, next) => {
+    const { idShipment } = req.params;
+    const { pickUpDireccion, pickUpProvince, deliveryDireccion, deliveryProvince, pallets } = req.body;
+
+    Shipment.findByIdAndUpdate(idShipment, { pickUpDireccion, pickUpProvince, deliveryDireccion, deliveryProvince, pallets }, { new: true })
+        .then(result => {
+            res.json(result);
+        })
+        .catch(err => next(err))
+});
+///api/projects/delete/:idProject
+router.delete("/delete/:id", (req, res, next) => {
+    const { idShipment } = req.params;
+    Shipment.findByIdAndDelete(idShipment)
+        .then(response => {
             res.json({ resultado: "ok" });
         })
         .catch(err => next(err))
