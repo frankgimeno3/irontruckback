b4fdf64a2fe4d5b602ffe4d2cc9763170bb04e0d
Ivan <Ivandgea@gmail.com>
2023-03-13 19:56:02 +0100

    registro de shipments done

--- routes/shipment.routes.js
+++ routes/shipment.routes.js
@@ -35,79 +35,76 @@ router.post("/new", (req, res, next) => {
 
     console.log(req.body)
 
-    Shipment.create({ author, creationDate, pickUpDireccion, pickUpProvince, deliveryDireccion, deliveryProvince, pallets })
+    Shipment.create({ author, creationDate, pickUpDireccion, pickUpProvince, deliveryDireccion, deliveryProvince, pallets, author })
         .then(response => {
 
-            console.log(response)
-
-
-            console.log(response)
-
-            res.json(response);
-        })
-        .catch(err => next(err))
-});
-// router.post("/new", (req, res, next) => {
-//     const { author, creationDate, pickUpDireccion, pickUpProvince, deliveryDireccion, deliveryProvince, pallets } = req.body;
-//     console.log(req.body)
-//     Shipment.create({ author, creationDate, pickUpDireccion, pickUpProvince, deliveryDireccion, deliveryProvince, pallets })
-//         // .then(response => {
-//         //     console.log(response)
-
-//         //     // return Sender.findByIdAndUpdate(author, { $push: { createdShipments: response } }, { new: true })
-//         // })
-//         .then((data) => {
-//              res.json(response) })
-
-//         .catch(err => next(err))
-// });
-
-// router.post("/new", (req, res, next) => {
-//     const { author, creationDate, pickUpDireccion, pickUpProvince, deliveryDireccion, deliveryProvince, pallets } = req.body;
-//     console.log(req.body)
-//     Shipment.create({ author, creationDate, pickUpDireccion, pickUpProvince, deliveryDireccion, deliveryProvince, pallets })
-//         // .then(response => {
-//         //     console.log(response)
-
-//         //     // return Sender.findByIdAndUpdate(author, { $push: { createdShipments: response } }, { new: true })
-//         // })
-//         .then((data) => {
-//              res.json(response) })
-
-//         .catch(err => next(err))
-// });
-
-
-router.put("/edit/:idshipment", (req, res, next) => {
-    const { idShipment } = req.params;
-    const { pickUpDireccion, pickUpProvince, deliveryDireccion, deliveryProvince, pallets } = req.body;
-
-    Shipment.findByIdAndUpdate(idShipment, { pickUpDireccion, pickUpProvince, deliveryDireccion, deliveryProvince, pallets }, { new: true })
-        .then(result => {
-            res.json(result);
-        })
-        .catch(err => next(err))
-});
-///api/projects/delete/:idProject
-router.delete("/delete/:id", (req, res, next) => {
-    const { idShipment } = req.params;
-    Shipment.findByIdAndDelete(idShipment)
         .then(response => {
-            res.json({ resultado: "ok" });
-        })
-        .catch(err => next(err))
-});
+            console.log(response)
 
-///api/projects/:idProject
-router.get("/:idShipment", (req, res, next) => {
-    const { idShipment } = req.params;
-    Shipment.findById(idShipment)
-        .populate("author")
-        .then(result => {
-            console.log("RESULT: ", result);
-            res.json(result);
         })
-        .catch(err => next(err))
-});
+                .catch(err => next(err))
+        });
+    // router.post("/new", (req, res, next) => {
+    //     const { author, creationDate, pickUpDireccion, pickUpProvince, deliveryDireccion, deliveryProvince, pallets } = req.body;
+    //     console.log(req.body)
+    //     Shipment.create({ author, creationDate, pickUpDireccion, pickUpProvince, deliveryDireccion, deliveryProvince, pallets })
+    //         // .then(response => {
+    //         //     console.log(response)
 
-module.exports = router;
+    //         //     // return Sender.findByIdAndUpdate(author, { $push: { createdShipments: response } }, { new: true })
+    //         // })
+    //         .then((data) => {
+    //              res.json(response) })
+
+    //         .catch(err => next(err))
+    // });
+
+    // router.post("/new", (req, res, next) => {
+    //     const { author, creationDate, pickUpDireccion, pickUpProvince, deliveryDireccion, deliveryProvince, pallets } = req.body;
+    //     console.log(req.body)
+    //     Shipment.create({ author, creationDate, pickUpDireccion, pickUpProvince, deliveryDireccion, deliveryProvince, pallets })
+    //         // .then(response => {
+    //         //     console.log(response)
+
+    //         //     // return Sender.findByIdAndUpdate(author, { $push: { createdShipments: response } }, { new: true })
+    //         // })
+    //         .then((data) => {
+    //              res.json(response) })
+
+    //         .catch(err => next(err))
+    // });
+
+
+    router.put("/edit/:idshipment", (req, res, next) => {
+        const { idShipment } = req.params;
+        const { pickUpDireccion, pickUpProvince, deliveryDireccion, deliveryProvince, pallets } = req.body;
+
+        Shipment.findByIdAndUpdate(idShipment, { pickUpDireccion, pickUpProvince, deliveryDireccion, deliveryProvince, pallets }, { new: true })
+            .then(result => {
+                res.json(result);
+            })
+            .catch(err => next(err))
+    });
+    ///api/projects/delete/:idProject
+    router.delete("/delete/:id", (req, res, next) => {
+        const { idShipment } = req.params;
+        Shipment.findByIdAndDelete(idShipment)
+            .then(response => {
+                res.json({ resultado: "ok" });
+            })
+            .catch(err => next(err))
+    });
+
+    ///api/projects/:idProject
+    router.get("/:idShipment", (req, res, next) => {
+        const { idShipment } = req.params;
+        Shipment.findById(idShipment)
+            .populate("author")
+            .then(result => {
+                console.log("RESULT: ", result);
+                res.json(result);
+            })
+            .catch(err => next(err))
+    });
+
+    module.exports = router;
