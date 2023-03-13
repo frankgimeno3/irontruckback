be4c7f1c32ef2cab663ad11b4e8661e5a0adac43
Eduardo Pizarro <eduardo-pizarro@hotmail.es>
2023-03-13 11:06:42 +0100

    con profile llamando a payload pero no a back

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -16,11 +16,15 @@ const Transportist = require("../models/Transportist.model");
 const Sender = require("../models/Sender.model");
 
 
-<<<<<<< HEAD
+const Transportist = require("../models/Transportist.model");
+const Sender = require("../models/Sender.model");
+
+
+const Transportist = require("../models/Transportist.model");
+const Sender = require("../models/Sender.model");
+
+
 // ********* require fileUploader in order to use it *********
-=======
-// // ********* require fileUploader in order to use it *********
->>>>>>> 4cb9fcc (to funciona de momento)
 const fileUploader = require("../config/cloudinary.config");
 const { isAuthenticated } = require("../middleware/jwt.middleware");
 
@@ -68,15 +72,30 @@ router.put("/:id", isAuthenticated, fileUploader.single("imageUrl"), (req, res, 
   const { email, name, phoneNumber, address, password, repeatPassword } = req.body;
   const updateFields = { email, name, phoneNumber, address, password, repeatPassword };
 
-  if (password !== repeatPassword) {
-    res.status(400).json({ message: "Password and repeat password must be the same" });
-    return;
-  }
+  // GET "/:id" => Route to your profile
+  router.get("/:id", isAuthenticated, (req, res, next) => {
+    const { id: idProject } = req.params;
+    // const { licensePlate } = user.licensePlate;
 
-  if (email === "" || password === "" || name === "" || phoneNumber === "" || address === "") {
-    res.status(400).json({ message: "Provide all fields" });
-    return;
-  }
+    if (!req.payload.isTransportist) {
+      Sender.findById({ idProject })
+        .then(result => {
+          res.json(result);
+        })
+        .catch(err => next(err))
+    }
+
+    if (licensePlate) {
+
+      Transportist.findById({ idProject })
+        .then(result => {
+          res.json(result);
+        })
+        .catch(err => next(err))
+    }
+  });
+
+
 
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
   if (!emailRegex.test(email)) {
@@ -137,28 +156,6 @@ if (licensePlate) {
     })
     .catch(err => next(err))
 }
-
-// // PUT /" => Route that receives the image, sends it to Cloudinary via the fileUploader and returns the image URL
-// router.put("/:id", isAuthenticated, fileUploader.single("imageUrl"), (req, res, next) => {
-//   const { idProject } = req.params;
-//   // console.log("file is: ", req.file)
-//   const { email, name, _id, phoneNumber, address } = req.body;
-//   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
-//   if (!emailRegex.test(email)) {
-//     res.status(400).json({ message: "Provide a valid email address." });
-//     return;
-//   }
-//   if (!phoneNumber.length === 9) {
-//     res.status(400).json({
-//       message:
-//         "The phoneNumber is not correct",
-//     });
-//     return;
-//   }
-//   if (!req.file) {
-//     next(new Error("No file uploaded!"));
-//     return;
-//   }
 
 // Get the URL of the uploaded file and send it as a response.
 // 'fileUrl' can be any name, just make sure you remember to use the same when accessing it on the frontend
