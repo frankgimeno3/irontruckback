bd74663c77b1f260b2439527b95d2e8e2cadf1fb
Eduardo Pizarro <eduardo-pizarro@hotmail.es>
2023-03-13 11:06:42 +0100

    con profile llamando a payload pero no a back

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -106,10 +106,10 @@ router.post("/login", (req, res, next) => {
 
       if (passwordCorrect) {
         // Deconstruct the sender object to omit the password
-        const { _id, email, password, name, image, phoneNumber, address, createdShipments, currentShipments, completedShipments, rejectedShipments } = foundSender;
+        const { _id, email, password, name, image } = foundSender;
 
         // Create an object that will be set as the token payload
-        const payload = { _id, email, password, name, image, phoneNumber, address, createdShipments, currentShipments, completedShipments, rejectedShipments };
+        const payload = { _id, name, email, isTransportist: false };
 
         // Create a JSON Web Token and sign it
         const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
