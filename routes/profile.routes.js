6a0d6d048c754c443b9e9cfc286f14d45909126f
Ivan <Ivandgea@gmail.com>
2023-03-14 11:22:08 +0100

    fasf

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,11 +1,17 @@
-const express = require("express");
-const router = express.Router();
+// const express = require("express");
+// const router = express.Router();
+// <<<<<<< HEAD
+// // import { createContext, useEffect, useState } from "react";
+// // import { AuthContext } from "../../irontruck/src/context/auth.context";
+// // const User = require("../models/User.model");
+// =======
 // import { createContext, useEffect, useState } from "react";
 // import { AuthContext } from "../../irontruck/src/context/auth.context";
 // const User = require("../models/User.model");
+// >>>>>>> 89d0d9a (domingo manyana antes de merge)
 
-const Transportist = require("../models/Transportist.model");
-const Sender = require("../models/Sender.model");
+// const Transportist = require("../models/Transportist.model");
+// const Sender = require("../models/Sender.model");
 
 
 const Transportist = require("../models/Transportist.model");
@@ -17,11 +23,11 @@ const fileUploader = require("../config/cloudinary.config");
 const { isAuthenticated } = require("../middleware/jwt.middleware");
 
 
-// useEffect(() => {
-//   authenticateUser();
-// }, []);
+// // useEffect(() => {
+// //   authenticateUser();
+// // }, []);
 
-// const {user} = useContext(AuthContext);
+// // const {user} = useContext(AuthContext);
 
 // GET "/:id" => Route to your profile
 router.get("/:id", isAuthenticated, (req, res, next) => {
@@ -130,21 +136,21 @@ router.put("/:id", isAuthenticated, fileUploader.single("imageUrl"), (req, res, 
   // Get the URL of the uploaded file and send it as a response.
   // 'fileUrl' can be any name, just make sure you remember to use the same when accessing it on the frontend
 
-  res.json({ fileUrl: req.file.secure_url });
-});
+//   res.json({ fileUrl: req.file.secure_url });
+// });
 
-// POST '/api/movies' => for saving a new movie in the database
-router.post('/movies', (req, res, next) => {
-  // console.log('body: ', req.body); ==> here we can see that all
-  // the fields have the same names as the ones in the model so we can simply pass
-  // req.body to the .create() method
+// // POST '/api/movies' => for saving a new movie in the database
+// router.post('/movies', (req, res, next) => {
+//   // console.log('body: ', req.body); ==> here we can see that all
+//   // the fields have the same names as the ones in the model so we can simply pass
+//   // req.body to the .create() method
 
-  Movie.create(req.body)
-    .then(createdMovie => {
-      // console.log('Created new movie: ', createdMovie);
-      res.status(200).json(createdMovie);
-    })
-    .catch(err => next(err));
-});
+//   Movie.create(req.body)
+//     .then(createdMovie => {
+//       // console.log('Created new movie: ', createdMovie);
+//       res.status(200).json(createdMovie);
+//     })
+//     .catch(err => next(err));
+// });
 
-module.exports = router; 
+// module.exports = router; 
